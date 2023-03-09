f5fdd4741a2a2e2c566304b4def1ab974678b42c
ZivLow <zivlowenhao@gmail.com>
2023-03-09 10:33:42 +0000

    update directory

--- desec_updater.sh
+++ desec_updater.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+# Get path of script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Check if the configuration file exists
-if [ -f deSEC_config ]; then
+if [ -f ${SCRIPT_DIR}/deSEC_config ]; then
     # Read the API key and domains from the configuration file
-    source deSEC_config
+    source ${SCRIPT_DIR}/deSEC_config
 else
     # Prompt for the API key and domains
     read -p "Enter deSEC API key: " api_key
     read -p "Enter deSEC domains (separated by space): " domains
 
     # Create config file
-    touch deSEC_config
+    touch ${SCRIPT_DIR}/deSEC_config
 
     # Save the API key and domains to the configuration file
-    echo "api_key=$api_key" >deSEC_config
-    echo "domains=($domains)" >>deSEC_config
+    echo "api_key=$api_key" > ${SCRIPT_DIR}/deSEC_config
+    echo "domains=($domains)" >> ${SCRIPT_DIR}/deSEC_config
 fi
 
 # Get the current IP address
