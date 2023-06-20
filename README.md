53c568a45030ba920b664228e41a2dec673a60fa
ZivLow <zivlowenhao@gmail.com>
2023-06-20 23:59:10 +0800

    Only ip_update when really needed. Logging with timestamp. ANSI colours on log output. Counter for no. of times IP updated and checked. README additions.

--- README.md
+++ README.md
@@ -1,22 +1,74 @@
 # deSEC IP Updater
 
-This script updates the IP address of multiple deSEC domains.
+Updates the IP address of multiple deSEC domains.
 
 ## Features
 - Prompts for deSEC API key and domains if a configuration file does not exist
 - Saves the API key and domains to a configuration file for future use
-- Fetches the current IP from api.ipify.org
+- Fetches the current IP using opendns via dig
 - Updates the IP address for each domain in the list
 - Waits 1 minute between each request to prevent rate-limiting
+- Only performs ip_update if public IP is different from the A record in deSEC name servers
+- Timestamping on log output
+- ANSI colour log output
+- Counter for the number of time ran IP has been checked and IP updates performed
 
 ## Usage
 1. Download or clone the script to your local machine
-2. Make the script executable by running `chmod +x desec_updater.sh`
-3. Run the script with `./desec_updater.sh`
-4. The script will prompt you for your deSEC API key and the list of domains to update
+```
+git clone git@github.com:ZivLow/desec-update.git
+cd desec-update
+```
+2. Make the script executable by running
+```
+sudo chmod +x desec_updater.sh ./config/colours.sh
+```
+3. Creating new configuration file
+```
+cd config
+cp deSEC_config_example deSEC_config
+```
+4. Put your deSEC API key and domains in configuration file.
+```
+nano deSEC_config
+```
+Exit & save with `CTRL+X`
+
+7. Set up a crontab job using:
+```
+crontab -e
+```
+8. Copy & paste crontab job from [crontab](config/crontab) file to your crontab.
+```
+*/3 * * * * ${HOME}/desec-update/desec_updater.sh
+```
+Save & exit with `CTRL+X`
+
+### To test running once (optional)
+1. Go to project root directory (~/desec-update/). \
+Run the script once with:
+```
+./desec_updater.sh
+```
+2. The script may prompt you for your deSEC API key and the list of domains to update
+
+## Logs
+- Log files will be written to `log/` folder
+- To view logs:
+```
+cat log/desec_updater_logs.log
+```
+- To view counters:
+```
+cat log/counter.log
+```
 
 ## Note
-- Make sure you have curl installed on your machine
+- Make sure you have dig, bash(>v4.3) installed on your machine
 - You can edit the script to change the wait time between requests
+- Tested to work on Ubuntu 22.04 LTS
 
+## Credits
+This script is originally forked from [A-Emile's deSEC-updater](https://github.com/A-Emile/deSEC-updater) repository.
+## Disclaimer
 This script is provided as is, use it at your own risk. 
--- desec_updater.sh
+++ desec_updater.sh
@@ -2,37 +2,206 @@
 
 # Get path of script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CONFIG_FILE_PATH="${SCRIPT_DIR}/config/deSEC_config"
+COLOURS_FILE_PATH="${SCRIPT_DIR}/config/colours.sh"
+LOG_DIR="${SCRIPT_DIR}/log"
+LOGS_FILE_PATH="${LOG_DIR}/desec_updater_logs.log"
+COUNTER_FILE_PATH="${LOG_DIR}/counter.log"
 
-# Check if the configuration file exists
-if [ -f ${SCRIPT_DIR}/deSEC_config ]; then
-    # Read the API key and domains from the configuration file
-    source ${SCRIPT_DIR}/deSEC_config
-else
-    # Prompt for the API key and domains
-    read -p "Enter deSEC API key: " api_key
-    read -p "Enter deSEC domains (separated by space): " domains
+### Functions ---------------------------------------------------------
 
-    # Create config file
-    touch ${SCRIPT_DIR}/deSEC_config
+# Log to console and to log file. Input $1 = string to log
+function log() {
+    local TIMESTAMP=$(date +"%r, %Z, %A, %e %B %Y")
+    echo -e $1 | (echo -n "[${TIMESTAMP}]    " && cat) |& tee -a "${LOGS_FILE_PATH}"
+}
 
-    # Save the API key and domains to the configuration file
-    echo "api_key=$api_key" > ${SCRIPT_DIR}/deSEC_config
-    echo "domains=($domains)" >> ${SCRIPT_DIR}/deSEC_config
-fi
-
-# Get the current IP address
-ip=$(curl -s https://api.ipify.org)
+# Get the public IP address
+function get_public_ip() {
+    [ -z ${ip+x} ] && ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+    echo ${ip}
+}
 
 # Update the IP address for each domain
-update_ip() {
-    for domain in "${domains[@]}"; do
-        echo "Updating $domain"
-        curl "https://update.dedyn.io/?hostname=$domain&myipv4=$ip" --header "Authorization: Token $api_key"
+function update_ip() {
+    local -n domains_local=$1
+    local api_key_local=$2
+
+    local ip=$(get_public_ip)
+
+    for domain in "${domains_local[@]}"; do
+        log "Updating $domain"
+        curl "https://update.dedyn.io/?hostname=$domain&myipv4=$ip" --header "Authorization: Token $api_key_local"
         echo
-        echo "Waiting 60 seconds"
+        log "Waiting 60 seconds"
         sleep 60
     done
 }
 
-# Run the script once now
-update_ip
+# Check if current public IP address is different from stored ip address
+# Return 0 = true (need to update)
+# Return 1 = false (no need to update)
+function need_update() {
+    local -n domains_local=$1
+    local -n name_servers_local=$2
+
+    local ip=$(get_public_ip)
+
+    for domain in "${domains_local[@]}"; do
+        for name_server in "${name_servers_local[@]}"; do
+            # Get the stored ip address for the domain in desec name server
+            domain_ip=$(dig +short $domain @${name_server})
+
+            # Check if current public IP address is same as the one from stored ip address
+            if [ "${ip}" == "${domain_ip}" ]; then
+                # No need to update the stored ip address for the current domain
+                log "${Green}Good match: Public IP ${ip} is recorded with domain ${UCyan}${domain}${Green} via ${name_server}${Color_Off}"
+                break
+            else
+                # Need to update the stored ip address for the current domain
+                log "${BRed}No match: ${Purple}Public IP ${ip} is different from ${domain_ip} recorded with domain ${UCyan}${domain}${Purple} via ${name_server}${Color_Off}"
+            fi
+
+            # If all name servers stored ip address is different from current public IP address
+            if [ "${name_server}" == "${name_servers[-1]}" ]; then
+                # Need to update the stored ip address. 0 = true
+                log "${On_Red}No match for all name servers:${Color_Off} ${IYellow}Need to update${Color_Off} ip address for ${UCyan}${domain}${Color_Off}"
+                return 0
+            fi
+        done
+    done
+
+    # No need to update the stored ip address for all domains. 1 = false
+    log "${On_IGreen}${BWhite}No updates needed for all domains.${Color_Off}"
+    return 1
+}
+
+# Function to change values in file, given a key.
+# $1 --> key	,	$2 --> new_value	,	$3 --> file path
+function edit_counter_file()
+{
+    sed -i -e "s/^\([[:space:]]*$1=\).*/\1$2/1" "$3"
+}
+
+# Read the value for a specified key in a file
+function read_properties() {
+    local search_key="$1"
+    local file="$2"
+
+    local key value
+    
+    while IFS="=" read -r key value; do
+        case "$key" in
+        '#'*) ;;
+        ${search_key})
+            read -d '' -r "${search_key}" <<< $(printf "$value")
+            ;;
+        esac
+    done < "$file"
+}
+
+# Update counter file
+function update_counter_file() {
+    local key=$1
+    local file_path=$2
+
+    local -i new_value=1
+
+    # Check if counter found
+    case $(grep -s "$key" "$file_path" >/dev/null; echo $?) in
+        0)
+            # If found
+            read_properties "${key}" "${file_path}"
+
+            # Increment value of key. ! is one level of indirection
+            new_value=$((${!key}+1))
+
+            # Edit counter file
+            edit_counter_file "${key}" "${new_value}" "${file_path}"
+            ;;
+        1)
+            # If not found
+            # Append update_ip_count to configuration file
+            echo "${key}=${new_value}" >> ${file_path}
+            ;;
+        *)
+            # If an error occurred (no such file)
+            log "${file_path} not found. Creating file..."
+            
+            # Append update_ip_count to configuration file
+            echo "${key}=${new_value}" >> ${file_path}
+            ;;
+    esac
+}
+
+# Update last check time
+function update_last_check_time() {
+    local key=$1
+    local file_path=$2
+
+    local TIMESTAMP=$(date +"%r, %Z, %A, %e %B %Y")
+
+    # Check if counter found
+    case $(grep -s "$key" "$file_path" >/dev/null; echo $?) in
+        0)
+            # If found
+            read_properties "${key}" "${file_path}"
+
+            # Edit counter file
+            edit_counter_file "${key}" "${TIMESTAMP}" "${file_path}"
+            ;;
+        1)
+            # If not found
+
+            # Append update_ip_count to configuration file
+            echo "${key}=${TIMESTAMP}" >> ${file_path}
+            ;;
+        *)
+            # If an error occurred (no such file)
+            log "${file_path} not found. Creating file..."
+            
+            # Append update_ip_count to configuration file
+            echo "${key}=${TIMESTAMP}" >> ${file_path}
+            ;;
+    esac
+}
+
+### Functions END ----------------------------------------------------
+
+
+### Program ----------------------------------------------------------
+
+# Check if log directory, log file, colours file exists
+[ -d "${LOG_DIR}" ] || mkdir "${LOG_DIR}"
+[ -f ${LOGS_FILE_PATH} ] && echo >> ${LOGS_FILE_PATH} || log "${LOGS_FILE_PATH} not found. Creating file..."
+[ -f ${COLOURS_FILE_PATH} ] && source ${COLOURS_FILE_PATH} || log "Warning: No colours.sh file found."
+
+# Check if the configuration file exists
+if [ -f ${CONFIG_FILE_PATH} ]; then
+    # Read the API key and domains from the configuration file
+    source ${CONFIG_FILE_PATH}
+else
+    # Prompt for the API key and domains
+    read -p "Enter deSEC API key: " api_key
+    read -p "Enter deSEC domains (separated by space): " domains
+    read -p "Enter name servers to check ip address stored for domains (separated by space): " name_servers
+
+    # Create config file
+    touch ${CONFIG_FILE_PATH}
+
+    # Save the API key and domains to the configuration file
+    echo "api_key=$api_key" > ${CONFIG_FILE_PATH}
+    echo "domains=($domains)" >> ${CONFIG_FILE_PATH}
+    echo "name_servers=($name_servers)" >> ${CONFIG_FILE_PATH}
+fi
+
+# Only update IP record on deSEC if need to update
+if need_update "domains" "name_servers"; then
+    update_ip "domains" "${api_key}"
+    update_counter_file "update_ip_count" "${COUNTER_FILE_PATH}"
+fi
+
+update_last_check_time "last_check_time" "${COUNTER_FILE_PATH}"
+update_counter_file "check_ip_count" "${COUNTER_FILE_PATH}"
+
+### Program END ------------------------------------------------------
